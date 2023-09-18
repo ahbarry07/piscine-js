4707488d2e984f9d067d5fd9b1a4a95e69fcbf16
ahbarry <barryahmadou135@gmail.com>
2023-09-18 21:32:01 +0000

    dog-years.js

--- dog-years.js
+++ dog-years.js
@@ -9,22 +9,22 @@ function dogYears(planet, age){
             return parseFloat(years.toFixed(2))
         case 'mercury':
             years = ((age / 0.2408467) / 31557600) * 7
-                return parseFloat(years.toFixed(2))
+            return parseFloat(years.toFixed(2))
         case 'venus':
             years = ((age / 0.61519726) / 31557600) * 7
-                return parseFloat(years.toFixed(2))
+            return parseFloat(years.toFixed(2))
         case 'mars':
             years = ((age / 1.8808158) / 31557600) * 7
-                return parseFloat(years.toFixed(2))
+            return parseFloat(years.toFixed(2))
         case 'jupiter':
             years = ((age / 11.862615) / 31557600) * 7
-                return parseFloat(years.toFixed(2))
+            return parseFloat(years.toFixed(2))
         case 'saturn':
             years = ((age / 29.447498) / 31557600) * 7
-                return parseFloat(years.toFixed(2))
+            return parseFloat(years.toFixed(2))
         case 'uranus':
             years = ((age / 84.016846) / 31557600) * 7
-                return parseFloat(years.toFixed(2))
+            return parseFloat(years.toFixed(2))
         case 'neptune':
             years = ((age / 164.79132) / 31557600) * 7
             return parseFloat(years.toFixed(2))
