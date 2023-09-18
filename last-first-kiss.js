6e9de2d6ef1082e80ffaf9c9db4e162e93bb8880
ahbarry <barryahmadou135@gmail.com>
2023-09-18 13:54:23 +0000

    last-first-kiss.js project

---
+++ last-first-kiss.js
@@ -0,0 +1,16 @@
+
+function first(str_or_arr){
+   return str_or_arr[0]
+}
+
+function last(str_or_arr){
+    return str_or_arr[str_or_arr.length-1]
+}
+
+function kiss(str_or_arr){
+    let array = []
+    array.push(str_or_arr[0], str_or_arr[str_or_arr.length-1])
+
+    return array
+}
+
