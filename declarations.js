74452a8bff656dc13ac906164131b16a8265b706
ahbarry <barryahmadou135@gmail.com>
2023-09-18 11:04:54 +0000

    Declaration.js file

---
+++ declarations.js
@@ -0,0 +1,23 @@
+const escapeStr = `\`\\/"'`
+Object.freeze(escapeStr)//Cette propriete permet de figer un objet c'est a dire de le rendre a ce qu'il ne puisse pas etre modifier
+
+const arr = [4, '2']
+Object.freeze(arr)
+
+const obj = {
+    str: "",
+    num: 0,
+    bool: false,
+    undef: undefined,
+}
+Object.freeze(obj)
+
+const nested = {
+    arr: [4, undefined, '2'],
+    obj:{
+        str:"",
+        num:0,
+        bool:false,
+    }
+}
+Object.freeze(nested)
