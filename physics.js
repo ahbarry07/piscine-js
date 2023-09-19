e3f75b5d6ee18f08a3cd79b3c4b9d02fac46471b
ahbarry <barryahmadou135@gmail.com>
2023-09-19 20:48:51 +0000

    physics.js

--- physics.js
+++ physics.js
@@ -1,9 +1,11 @@
 
 function getAcceleration(data){
 
-    if ((Number.isNaN(data.f)) || (Number.isNaN(data.m)) || data.m === 0 || data.length === 0){
+    if (data.length === undefined || (Number.isNaN(data.f)) || (Number.isNaN(data.m)) || data.m === 0){
         return "impossible"
     }
-
+   
     return data.f/data.m
 }
+
+console.log(getAcceleration({}))
