71c96cc7b7f2adfbbbaa3269a15160a73d0d35fb
ahbarry <barryahmadou135@gmail.com>
2023-09-19 13:35:29 +0000

    abs.js...

--- abs.js
+++ abs.js
@@ -5,7 +5,7 @@ function abs(number){
     if (number >= 0){
         return number
     }else{
-        return number.toString().slice(1)
+        return parseInt(number.toString().slice(1))
     }
 }
 
