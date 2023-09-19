4a496ecde15e44ff9ce6405ac4e48948ca09f24f
ahbarry <barryahmadou135@gmail.com>
2023-09-19 15:49:10 +0000

    is.js

---
+++ is.js
@@ -0,0 +1,45 @@
+const is = {}
+
+is.num = function (num){
+    return (typeof num) === "number"
+}
+
+is.nan = function (nan){
+    return isNaN(nan)
+}
+
+is.str = function (str){
+    return (typeof str) === "string"
+}
+
+is.bool = function (bool){
+    return (typeof bool) === "boolean"
+}
+
+is.undef = function (und){
+    return und === undefined
+}
+
+is.def = function (deff){
+    return deff !== undefined
+}
+
+is.arr = function (array){
+    return Array.isArray(array) 
+}
+
+is.obj = function (objet){
+    return (typeof objet) === "object"
+}
+
+is.fun = function (func){
+    return (typeof func) === "function"
+}
+
+is.truthy = function (truth){
+    return (typeof truth) === "truthy"
+}
+
+is.falsy = function (fals){
+    return (typeof fals) === "falsy"
+}
