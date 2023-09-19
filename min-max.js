5aedcd0e3655a7fef177ea0da8b81f0addeebe75
ahbarry <barryahmadou135@gmail.com>
2023-09-19 13:51:05 +0000

    min-max.js

---
+++ min-max.js
@@ -0,0 +1,20 @@
+
+function max(num1, num2){
+    if (num1 > num2){
+        return num1
+    }else if (num1 < num2){
+        return num2
+    }else{
+        return 0
+    }
+}
+
+function min(num1, num2){
+    if (num1 < num2){
+        return num1
+    }else if (num1 > num2){
+        return num2
+    }else{
+        return 0
+    }
+}
