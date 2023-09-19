5867f11ba8ab0a9f5c0f6a13721963f948aca3a0
ahbarry <barryahmadou135@gmail.com>
2023-09-19 22:18:08 +0000

    physics.js

--- physics.js
+++ physics.js
@@ -1,14 +1,24 @@
 
 function getAcceleration(data){
 
-    if (data == {} || (Number.isNaN(data.f)) || (Number.isNaN(data.m)) || data.m === 0){
+    if ((Number.isNaN(data.f) || Number.isNaN(data.m)) && (Number.isNaN(data.Δv) || Number.isNaN(data.Δt)) && (Number.isNaN(data.d) || Number.isNaN(data.t))){
         return "impossible"
     }
-   
-    let acc = data.f/data.m
-    if (Number.isNaN(acc)){
+    let acc1 = data.f/data.m
+    let acc2 = data.Δv/data.Δt
+    let acc3 = (2*data.d)/(data.t**2)
+
+    if (!Number.isNaN(acc1)){
+        return acc1
+    }else if (Number.isNaN(acc1) && !Number.isNaN(acc2)){
+        return acc2
+    }else if (!Number.isNaN(acc2) && Number.isNaN(acc3)){
+        return acc3
+    }else if (Number.isNaN(acc1) && Number.isNaN(acc2) && !Number.isNaN(acc3)){
+        return acc3
+    }else {
         return "impossible"
     }
-    return  acc
+    
 }
 
