64d95a60022731b20d21537627f90573afedd358
ahbarry <barryahmadou135@gmail.com>
2023-09-19 13:23:24 +0000

    method-man.js

---
+++ method-man.js
@@ -0,0 +1,11 @@
+
+const words = (str) => str.split(' ')
+
+const sentence = (arr) => arr.join(' ')
+
+const yell = (str) => str.toUpperCase()
+
+const whisper = (str) => "*"+str.toLowerCase()+"*"
+
+const capitalize = (str) => str[0].toUpperCase() + str.toLowerCase().slice(1)
+
