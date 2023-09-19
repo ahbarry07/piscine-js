b3a963a1c1378db088cc7207d486a4b9380b836d
ahbarry <barryahmadou135@gmail.com>
2023-09-19 14:01:47 +0000

    sign.js

---
+++ sign.js
@@ -0,0 +1,17 @@
+
+function sign(number){
+    if (number > 0){
+        return 1
+    }else if (number < 1){
+        return -1
+    }
+    return 0
+}
+
+function sameSign(num1, num2){
+    if ((num1 > 0 && num2 > 0) || (num1 < 0 && num2 < 0)){
+        return true
+    }
+
+    return false
+}
