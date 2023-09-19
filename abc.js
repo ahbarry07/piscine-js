62d3e414d3bdd9e17e242e5b0da9e5e32c3fbc72
ahbarry <barryahmadou135@gmail.com>
2023-09-19 13:34:21 +0000

    abs.js

---
+++ abc.js
@@ -0,0 +1,12 @@
+
+const isPositive = (number) => number > 0
+
+function abs(number){
+    if (number >= 0){
+        return number
+    }else{
+        return number.toString().slice(1)
+    }
+}
+
+
