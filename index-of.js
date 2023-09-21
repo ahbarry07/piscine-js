915e8f0b86b6930d0a89642bedc840db0ea5e840
ahbarry <barryahmadou135@gmail.com>
2023-09-21 10:31:41 +0000

    index-of.js

--- index-of.js
+++ index-of.js
@@ -18,15 +18,20 @@ function lastIndexOf(array, occ){
     let ind = 0
     if (arguments.length === 2){
         ind = 0
+        for(let i = array.length; i >= ind; i--){
+            if (array[i] === occ){
+               return i
+            }
+        }
     }else if (arguments.length === 3){
         ind = parseFloat(arguments[2])
-    }
-
-    for(let i = ind; i >= 0; i--){
-        if (array[i] === occ){
-           return i
+        for(let i = ind; i >= 0; i--){
+            if (array[i] === occ){
+               return i
+            }
         }
     }
+
     return -1
 }
 
@@ -46,4 +51,4 @@ function includes(array, occ){
     return false
 }
 
-console.log(lastIndexOf(['t', 0, 0, 't'], 't', 2))
+console.log(lastIndexOf([1, 2, 3, 4, 5, 4, 3, 2, 1], 2))
