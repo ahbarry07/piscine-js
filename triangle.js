f3bac388ba9fe1dfa0102404c251fa15927fe390
ahbarry <barryahmadou135@gmail.com>
2023-09-22 16:18:23 +0000

    triangle.js

---
+++ triangle.js
@@ -0,0 +1,22 @@
+
+function triangle(elem, heigth){
+
+    let str = ""
+    let j = 1
+    for( let i = 1; i <= heigth; i++){
+        while(j<=2*i-1)
+        {
+            str += elem
+            j++;
+        }
+        if (i < heigth){
+            str+="\n"
+        }
+        j = 1 
+        
+    }
+    console.log(str)
+}
+
+
+(triangle("*", 5))
