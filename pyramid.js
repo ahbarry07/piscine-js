99077a7bc412be54399e55fd77c13643d60f5506
ahbarry <barryahmadou135@gmail.com>
2023-09-22 17:06:44 +0000

    pyramid.js

---
+++ pyramid.js
@@ -0,0 +1,22 @@
+
+function pyramid(elem, size){
+
+    let j = 0
+    let str =  ""
+    for (let i = 1; i <= size; i++){
+        for (let k = 1; k <= size-i; k++){
+            str += " ".repeat(elem.length) 
+        }
+        while(j < 2*i-1){
+            str += elem + ""
+            j++ 
+        }
+        j = 0
+        if (i<size){
+            str += "\n"
+        }
+    }
+
+    console.log(str)
+}
+
