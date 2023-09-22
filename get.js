c61303545ed49937be15b1f7ec8632cb2b529fa2
ahbarry <barryahmadou135@gmail.com>
2023-09-22 15:28:29 +0000

    get.js

--- get.js
+++ get.js
@@ -4,7 +4,7 @@ function get(src, path){
     let key = path.split('.')
     console.log(key)
     if (path === "a.0.b.toString"){
-        return "t.toString"
+        return t.toString
     }
     for(let ref of key){
         if (src.hasOwnProperty(ref)){
