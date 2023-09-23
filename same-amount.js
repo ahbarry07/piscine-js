bad6b6b1083a746915f3387a90d0e0ea0258187b
ahbarry <barryahmadou135@gmail.com>
2023-09-23 21:02:38 +0000

    same-amount.js

--- same-amount.js
+++ same-amount.js
@@ -1,8 +1,8 @@
 
 function sameAmount(str, regexp1, regexp2){
 
-    const matches1 = inputString.match(regexp1)
-    const matches2 = inputString.match(regexp2)
+    const matches1 = str.match(regexp1)
+    const matches2 = str.match(regexp2)
   
     // Check if both matches are null (no matches) or have the same length
     return matches1 === matches2
