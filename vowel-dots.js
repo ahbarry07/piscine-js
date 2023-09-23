24165c00cfa7676f4e1f1c5883057d1046ec06d2
ahbarry <barryahmadou135@gmail.com>
2023-09-23 20:06:18 +0000

    vowel-dots.js

--- vowel-dots.js
+++ vowel-dots.js
@@ -1,21 +1,8 @@
 
 function vowelDots(str){
 
-    const vowels = /[aeiou]/g
-    let [newStr, match] = ["", ""]
-
-    if (str.match(vowels) !== null){
-         match = str.match(vowels) 
-    }
-
-    for (let i= 0; i< str.length; i++){
-        newStr += str[i]
-        for(let res of match){
-            if (str[i] === res){
-                newStr += '.'
-            }
-        }
-    }
-    return newStr
+    const vowels = /[aeiouAEIOU]/g
+    
+    const result = str.replace(vowels, (match) => match + '.')
 }
 
