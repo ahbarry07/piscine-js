1f721f077b56e699e00ad3be8a02a9f44f60cf3c
ahbarry <barryahmadou135@gmail.com>
2023-09-23 21:24:46 +0000

    same-amount.js

--- same-amount.js
+++ same-amount.js
@@ -1,16 +1,8 @@
 
 function sameAmount(str, regexp1, regexp2){
 
-    const match1 = str.match(regexp1)
-    const match2 = str.match(regexp2)
+    const match1 = (str.match(regexp1) || []).length; // Utilisation de [].length pour éviter les erreurs si match1 est null
+    const match2 = (str.match(regexp2) || []).length; // Utilisation de [].length pour éviter les erreurs si match2 est null
     
-    if (regexp1 == /q / && regexp2==/qqqqqqq/){
-        return false
-    }
-    if (match1 && match2){
-        return match1.length === match2.length
-    }else if (!match1 && !match2){
-        return true
-    }
-    return false
+    return match1 === match2;
 }
