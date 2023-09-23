09d750e20a6b8fd807d9f6127ca0b3bd2bf23b13
ahbarry <barryahmadou135@gmail.com>
2023-09-23 20:42:43 +0000

    same-amount.js

---
+++ same-amount.js
@@ -0,0 +1,13 @@
+
+function sameAmount(str, regexp1, regexp2){
+
+    const match1 = str.match(regexp1)
+    const match2 = str.match(regexp2)
+    
+    if (match1 && match2){
+        return match1.length === match2.length
+    }else if (!match1 && !match2){
+        return true
+    }
+    return false
+}
