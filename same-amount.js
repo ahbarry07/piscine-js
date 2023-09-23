47723d668bd3f2eb6f90ceb52e69d2f8a588d99c
ahbarry <barryahmadou135@gmail.com>
2023-09-23 21:25:39 +0000

    same-amount.js

--- same-amount.js
+++ same-amount.js
@@ -1,8 +1,9 @@
 
 function sameAmount(str, regexp1, regexp2){
 
-    const match1 = (str.match(regexp1) || []).length; // Utilisation de [].length pour éviter les erreurs si match1 est null
-    const match2 = (str.match(regexp2) || []).length; // Utilisation de [].length pour éviter les erreurs si match2 est null
+    const match1 = (str.match(regexp1) || []);
+    const match2 = (str.match(regexp2) || []);
     
-    return match1 === match2;
+    return match1.length === match2.length;
+
 }
