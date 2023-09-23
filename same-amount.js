d06e2a8180f1b7e3a968d7064f753a5b8464b4a3
ahbarry <barryahmadou135@gmail.com>
2023-09-23 20:50:04 +0000

    same-amount.js

--- same-amount.js
+++ same-amount.js
@@ -4,10 +4,17 @@ function sameAmount(str, regexp1, regexp2){
     const match1 = str.match(regexp1)
     const match2 = str.match(regexp2)
     
-    if (match1 && match2){
-        return match1.length === match2.length
+    if (match1 && match2){//verifier si les tableaux correspond
+        if (match1.length === match2.length){ // Vérifier si les tableaux ont la même longueur
+            for (let i = 0; i < match1.length; i++) {
+                if (match1[i] !== match2[i]) {//comparer chaque correspondance
+                  return false;
+                }
+            }
+            return true
+        } 
     }else if (!match1 && !match2){
-        return match1.length === match2.length
+        return true
     }
     return false
 }
