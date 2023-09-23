d6a1531683c127caa5dc91d1f9848562c1427c17
ahbarry <barryahmadou135@gmail.com>
2023-09-23 20:59:53 +0000

    same-amount.js

--- same-amount.js
+++ same-amount.js
@@ -1,9 +1,12 @@
 
 function sameAmount(str, regexp1, regexp2){
 
-    const count1 = (inputString.match(regexp1) || []).length;
-    const count2 = (inputString.match(regexp2) || []).length;
-
-  // Vérifier si les deux regex ont le même nombre de correspondances
-  return count1 === count2;
+    const matches1 = inputString.match(regexp1);
+    const matches2 = inputString.match(regexp2);
+  
+    // Check if both matches are null (no matches) or have the same length
+    return (
+      (matches1 === null && matches2 === null) ||
+      (matches1 !== null && matches2 !== null && matches1.length === matches2.length)
+    );
 }
