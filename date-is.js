6522baea8819bad95c75bcdc413b6c01d110e0bf
ahbarry <barryahmadou135@gmail.com>
2023-09-25 21:17:38 +0000

    last date-is.js

--- date-is.js
+++ date-is.js
@@ -9,6 +9,6 @@ const isBefore = (date1, date2) => (new Date (date1) < new Date(date2))
 
 const isFuture = (date) => Date.now() < new Date(date).getTime()
 
-const  isPast = (date) =>isValid(date) && isBefore(date)
+const  isPast = (date) =>isValid(date) && isBefore(date, Date.now())
 
-console.log(isPast('2013-01-01'))
+console.log(isPast(new Date(1442, 11, 31)))
