0e91d716ebf3ebe4ca0d75335ff592d2f520a95f
ahbarry <barryahmadou135@gmail.com>
2023-09-25 19:51:39 +0000

    dr-strange.js

--- dr-strange.js
+++ dr-strange.js
@@ -1,42 +1,32 @@
+const dateRef = new Date('0001-01-01')
 
-function dayWeek(day){
-
+function addWeek(date) {
+ 
     const daysOfWeek = new Map()
     let days = ['Monday', 'Thuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'secondMonday', 'secondThuesday', 'secondWednesday', 'secondThursday', 'secondFriday', 'secondSaturday', 'secondSunday']
     for(let i = 0; i <= days.length-1; i++){
-        daysOfWeek.set(i+1, days[i])
+        daysOfWeek.set(i, days[i])
     }
 
+    let datDiff = ((date - dateRef) / (24 * 60 * 60 * 1000))
+    let datWeek = datDiff % 14
+    console.log(datWeek)
     for(let [key, value] of daysOfWeek){
-        if (day === key){
+        if (datWeek === key){
             return value
         }
     }
     return ""
 }
 
-function addWeek(date) {
- 
-    let day = date.getDate()   
-    
-   if (day === 31){
-        day = 1
-   }else{
-        day += 1
-   }
-   if (day <= 14){
-        return dayWeek(day)
-   }else{
-        return dayWeek(day - 14)
-   }
-}
-
 
 function timeTravel(Obj){
 
-    let newHoure = Obj.setHours()
+    // let date = new Date(Obj)
+    Obj.hour.setHours()
+    Obj.minute.setMinutes()
+    Obj.second.setSeconds
 
-    return newHoure
-
+    let date = new Date (Obj)
+    return date
 }
-
