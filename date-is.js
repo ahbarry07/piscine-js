8f5b5d2896c4a0714104191810cebcd2fc030b5a
ahbarry <barryahmadou135@gmail.com>
2023-09-25 21:04:24 +0000

    date-is.js

--- date-is.js
+++ date-is.js
@@ -1,5 +1,7 @@
 
-const  isValid = (date) => !isNaN((date)) || (date instanceof Date)
+const  isValid = (date) => {
+    return date !== null && date !== undefined && date instanceof Date && date.toString() !== 'Invalid Date' || Number.isInteger(date)
+}  
 
 const isAfter = (date1, date2) => (new Date(date1) > new Date(date2))
 
@@ -9,4 +11,4 @@ const isFuture = (date) => Date.now() > new Date(date)
 
 const  isPast = (date) => Date.now() < new Date(date) 
 
-console.log(isValid(1488370835081))
+console.log(isValid(new Date('1995-12-17T03:24:00').getTime()))
