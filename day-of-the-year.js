183c0a95676c58220412a32312b101b7c2993a0f
ahbarry <barryahmadou135@gmail.com>
2023-09-26 12:41:51 +0000

    day-of-the-year.js

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -7,7 +7,6 @@ function dayOfTheYear(date){
     let padStart = year.toString().padStart(4, '0')
     let origineDate = (padStart+'-01-01')
     let origine = new Date(origineDate)
-    origine = getTime()
     const timeDifference = date - origine;
     let numberOfday = Math.floor(timeDifference / (24*60*60*1000))
 
