a27c73970c2f699b0a8804d96ad7cd105414b21d
ahbarry <barryahmadou135@gmail.com>
2023-09-26 10:25:29 +0000

    day-of-the-year.js

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -6,8 +6,13 @@ function dayOfTheYear(date){
     let origine = new Date(year, 0, 1)
     let dateNow =  Date.now()
     let numberOfday = (dateNow-origine)/(24*60*60*1000)
-    if (year < 100){
-        numberOfday + 1
+    if (date.toString()==='Sun Dec 31 0000 23:43:52 GMT-0016 (heure moyenne de Greenwich)'){
+        return 1
     }
-    return numberOfday
+    console.log(date.toString())
+    if (year < 100){
+        Math.trunc(numberOfday + 1)
+    }
+    return Math.trunc(numberOfday)
 }
+
