6e0739546a8bed5cb5c2f978f6c32013cad2a237
ahbarry <barryahmadou135@gmail.com>
2023-09-26 14:01:58 +0000

    sunny-sunday.js

--- sunny-sunday.js
+++ sunny-sunday.js
@@ -1,10 +1,9 @@
 
 function sunnySunday(inputDate) {
-    
+
     let year = inputDate.getFullYear() 
     
     const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-    let dayOfWeek = inputDate.getDay()
     let newDate = new Date (year, 0, 1)
     newDate.setFullYear('0001')
     let adjustedDayOfWeek = Math.ceil((inputDate-newDate)/ (24 * 60 * 60 * 1000))
