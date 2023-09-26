c4f3b345a30603714ee04dd996cdf2bc1e1f6932
ahbarry <barryahmadou135@gmail.com>
2023-09-26 04:22:21 +0000

    get-some-time.js

--- get-some-time.js
+++ get-some-time.js
@@ -2,16 +2,16 @@
 function firstDayWeek(week, year){
 
     const regex = /\//g
-
-    let date = new Date(year)
-    let annee = new Date(parseInt(year), 0, 1)
-	let jour = date.getDay()
-    if (week ===1 && jour !==1){
+   
+    year = parseInt(year)
+    let annee = new Date(year, 0, 1)
+    date = annee.getFullYear()
+	let jour = annee.getDay()
+    if (week === 1 && jour !==1){
         let dateFormat = new Intl.DateTimeFormat('fr-FR').format(annee)
         let lastFormat = dateFormat.replace(regex, '-')
         return lastFormat
     }
-    console.log(month)
     let count = 0
     if (jour > 1){
         for(let i=2; i < 7; i++){
@@ -22,13 +22,20 @@ function firstDayWeek(week, year){
           }
         }
     }else{
-        count = 1
+        count = 6
     }
     let newDat = ((week-1)*7)-count
     annee.setDate(annee.getDate() + newDat)
-
+    if (year < 100){
+        annee.setFullYear(year)
+    }
     let dateFormat = new Intl.DateTimeFormat('fr-FR').format(annee)
     let lastFormat = dateFormat.replace(regex, '-')
+    while (lastFormat.slice(6).length < 4){
+        lastFormat = lastFormat.split('-')
+        lastFormat[2]='0'+lastFormat[2]
+        lastFormat = lastFormat.join('-')
+    }
     return lastFormat
     
 }
