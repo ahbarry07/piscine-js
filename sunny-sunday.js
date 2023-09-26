e6f0138c6b5c136172abd3cb450ecc44d70a17e6
ahbarry <barryahmadou135@gmail.com>
2023-09-26 13:34:45 +0000

    sunny-sunday.js

--- sunny-sunday.js
+++ sunny-sunday.js
@@ -3,7 +3,8 @@ function sunnySunday(inputDate) {
 
     const weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     const dayOfWeek = inputDate.getDay()
-    const adjustedDayOfWeek = (dayOfWeek + 6) % 7
+    const adjustedDayOfWeek = (dayOfWeek + 6) % 6
     return weekdays[adjustedDayOfWeek]
 }
 
+console.log(sunnySunday(new Date('0001-01-07')))
