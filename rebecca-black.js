924ce2ef46c36808b8cc0815f0c9738ce7c380ad
ahbarry <barryahmadou135@gmail.com>
2023-09-26 08:26:58 +0000

    rebecca-black.js

--- rebecca-black.js
+++ rebecca-black.js
@@ -11,10 +11,22 @@ const isLeapYear = (date) =>{
     return ((nexYear-newDate)/(24 * 60 * 60 * 1000)) === 366
 }
 
-const isLastDayOfMonth = (date) =>  date.getDate() === 31 && checkMonth(date.getMonth()) ? true 
-    : date.getDate() === 30 && !checkMonth(date.getMonth()) ? true 
-    : date.getMonth() === 1 && (date.getDate() === 28 || date.getDate() === 29) ? true : false
+const isLastDayOfMonth = (date) =>  {
+    
+    if ((date.getDate() === 31 && checkMonth(date.getMonth())) || (date.getDate() === 30 && !checkMonth(date.getMonth())) ){return true}  
+   
+    if (date.getMonth() === 1)  {
+        let year = date.getFullYear() 
+        let newDate = new Date(year, 0, 1)
+        let nexYear = new Date(year+1, 0, 1)
+        
+        if ((((nexYear-newDate)/(24 * 60 * 60 * 1000)) === 366 && date.getDate() === 29) ||(((nexYear-newDate)/(24 * 60 * 60 * 1000)) === 365 && date.getDate() === 28) ){
+            return true
+        }
+    }  
 
+    return false
+}
 
 const checkMonth = (month) =>{
     if (month===0 ||  month===2 || month===4 || month===6 || month===8 || month===9 || month===11){
