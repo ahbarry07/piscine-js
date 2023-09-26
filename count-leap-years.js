f0bfaa41e1a5a93d73693e7d0228807ffde0df0f
ahbarry <barryahmadou135@gmail.com>
2023-09-26 10:01:31 +0000

    count-leap-years.js

--- count-leap-years.js
+++ count-leap-years.js
@@ -4,8 +4,6 @@ function countLeapYears(date){
     let count = 0
     let year = date.getFullYear() 
     for (let i=1; i<=year+1; i++){
-        console.log(new Date(i, 0, 1))
-        console.log(new Date(i+1, 0, 1))
         if (((new Date(i + 1, 0, 1)) -(new Date(i, 0, 1)))/(24*60*60*1000) === 366 && i !== year+1){
             count++
         }
@@ -13,3 +11,4 @@ function countLeapYears(date){
   return count
 }
 
+console.log(countLeapYears(new Date('1664-08-09')))
