8ec59b11027b1321336fd5281836a38ea829b69a
ahbarry <barryahmadou135@gmail.com>
2023-09-27 12:50:05 +0000

    using-filter.js lasst

--- using-filter.js
+++ using-filter.js
@@ -1,6 +1,6 @@
 const regex1 = /[aoieuAOUIE]/ //recherche si une chaine contient une voyelle
 const regex2 = /(?:[^aeiouAEIOU]*[aeiouAEIOU]){5,}/ // la chaine doit contenir au moins 5 voyelles
-const regex3 = /aeiouAEIOU/ // la chaine ne doit pas commencer par une voyelle 
+const regex3 = /^[^aeiouAEIOU]/ // la chaine ne doit pas commencer par une voyelle 
 
 const filterShortStateName = (array) => array.filter((x) => x.length < 7 )
 
@@ -19,9 +19,10 @@ function filter1DistinctVowel(arr) {
 function multiFilter(arrayObj){
     return arrayObj.filter(temp =>{
         let capital = temp.capital.length >= 8
-        let name = !temp.name[0].match(regex3)
+        let name = temp.name.match(regex3)
         let tag = temp.tag.match(/(?:[^aeiouAEIOU]*[aeiouAEIOU]){1,}/) 
         let region = temp.region !== "South"
         return capital && name && tag && region
     })
 }
+
