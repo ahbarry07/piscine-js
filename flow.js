ad209b569534f8435e2d5e10bf6835dbb28f4d64
ahbarry <barryahmadou135@gmail.com>
2023-09-27 23:07:20 +0000

    flow.js

--- flow.js
+++ flow.js
@@ -3,8 +3,8 @@ function flow(funcs) {
     return function (...args) {
       let result = args;
       for (const func of funcs) {
-        result = func(...result);
-  
+        if (Array.isArray(result))  result = func(...result);
+       
         if (result < 0) {
           return 0;
         }
