fbeab2f2839d9f5da99fb5ac2b5b6aaf479b8dc1
ahbarry <barryahmadou135@gmail.com>
2023-09-27 21:44:50 +0000

    quantifiers.js

--- quantifiers.js
+++ quantifiers.js
@@ -12,7 +12,7 @@ function some(array, func){
     for (let i = 0; i < array.length; i++){
          if (func(array[i], i, array)) count ++
     }
-    return count !== 0
+    return count > 0
 }
 
 function none(array, func){
