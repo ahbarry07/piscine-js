ce997ea622970266f84e966c34f4d125dc817f00
ahbarry <barryahmadou135@gmail.com>
2023-09-27 13:41:24 +0000

    using-reduce.js

--- using-reduce.js
+++ using-reduce.js
@@ -5,7 +5,3 @@ const sumOrMul = (array, num) => array.reduce((sum, currentValue) => currentValu
 
 const funcExec = (functions, initial) => functions.reduce((acc, func) => func(acc), initial);
 
-const array1 = [1, 2, 3, 5, 8];
-console.log(sumOrMul(array1, 5))
-console.log(adder(array1))
-
