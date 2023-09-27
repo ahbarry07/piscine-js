219e7eafcf7c3a160e131df03a87af7f45ccc5e8
ahbarry <barryahmadou135@gmail.com>
2023-09-27 14:01:43 +0000

    using-reduce.js

--- using-reduce.js
+++ using-reduce.js
@@ -1,8 +1,7 @@
 
 const adder = (array, initial=0) => array.reduce((sum, currentValue) => sum + currentValue, initial)
 
-const sumOrMul = (array, num) =>{
-
+const sumOrMul = (array, num =0) =>{
     return array.reduce((sum, currentValue) => {
         if (currentValue % 2 === 0) sum *= currentValue 
         else sum += currentValue
