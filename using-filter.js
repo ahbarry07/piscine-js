7a0e88c1a867e470639e660e3230ca013ed7091e
ahbarry <barryahmadou135@gmail.com>
2023-09-27 11:49:38 +0000

    using-filter.js

---
+++ using-filter.js
@@ -0,0 +1,28 @@
+const regex1 = /[aoieuAOUIE]/ //recherche si une chaine contient une voyelle
+const regex2 = /(?:[^aeiouAEIOU]*[aeiouAEIOU]){5,}/ // la chaine doit contenir au moins 5 voyelles
+const regex3 = !/^aeiouAEIOU/ // la chaine ne doit pas commencer par une voyelle 
+
+const filterShortStateName = (array) => array.filter((x) => x.length === 7 )
+
+const filterStartVowel = (array) => array.filter((x) => x[0].match(regex1))
+
+const filter5Vowels = (array) => array.filter((value) => value.match(regex2))
+
+function filter1DistinctVowel(arr) {
+    return arr.filter(word => {
+      const lowercaseWord = word.toLowerCase()
+      const vowels = [...lowercaseWord].filter(char => 'aeiou'.includes(char))
+      return new Set(vowels).size === 1
+    })
+}
+
+function multiFilter(arrayObj){
+    arrayObj.filter(temp =>{
+        temp.capital.length === 8
+        temp.name.match(regex3)
+        temp.tag.match(/(?:[^aeiouAEIOU]*[aeiouAEIOU]){1,}/)
+        temp.region !== "South"
+    })
+    return temp
+}
+
