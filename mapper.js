05391868549045904e1c6bdf98bb97da8f66bee1
ahbarry <barryahmadou135@gmail.com>
2023-09-27 15:30:36 +0000

    mapper.js

--- mapper.js
+++ mapper.js
@@ -13,9 +13,7 @@ function flatMap(array, func){
     let newArray = []
     for (let i = 0; i < array.length; i++){
         let stock = func(array[i], i, array)
-        for(let j = 0; j < stock.length; j++){
-            newArray.push(stock[j])
-        }
+        newArray.push(...stock)
     }
     return newArray
 }
