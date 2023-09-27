16738eb6c8af2b78fbd75472b1fa87433ca348e3
ahbarry <barryahmadou135@gmail.com>
2023-09-27 19:56:06 +0000

    filter.js

---
+++ filter.js
@@ -0,0 +1,33 @@
+
+function filter(array, func){
+    let newArray = []
+    for(let i = 0; i < array.length; i++){
+        if (func(array[i])){
+            newArray.push(array[i])
+        }
+    }
+    return newArray
+}
+
+function reject(array, func){
+    let newArray = []
+    for(let i = 0; i < array.length; i++){
+        if (!func(array[i])){
+            newArray.push(array[i])
+        }
+    }
+    return newArray
+}
+
+function partition(array, func){
+    let trueArray = []
+    let falseArray = []
+    for(let i = 0; i < array.length; i++){
+        if (func(array[i])){
+            trueArray.push(array[i])
+        }else{
+            falseArray.push(array[i])
+        }
+    }
+    return [trueArray, falseArray]
+}
