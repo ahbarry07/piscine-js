55e13a2c21f7da608e9bbd9f418be2020482187b
ahbarry <barryahmadou135@gmail.com>
2023-09-27 22:09:10 +0000

    sweet-curry.js

--- sweet-curry.js
+++ sweet-curry.js
@@ -3,6 +3,6 @@ const returnNumber = (number) => number
 
 const mult2 = (number) => returnNumber(number) * returnNumber(number)
 
-const add = (number) => returnNumber(number) + returnNumber(number) + returnNumber(number)
+const add3 = (number) => returnNumber(number) + returnNumber(number) + returnNumber(number)
 
 const sub4 = (number) => returnNumber(number) - returnNumber(number) - returnNumber(number) - returnNumber(number)
