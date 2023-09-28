02b3f7cc1c4f7545a4ce919f464068d3cc4d9405
ahbarry <barryahmadou135@gmail.com>
2023-09-28 22:27:14 +0000

    build-brick-and-break.js

---
+++ build-brick-and-break.js
@@ -0,0 +1,42 @@
+
+function build(numOfBric){
+ let i = 1
+   let timer= setInterval(function(){
+        let div = document.createElement('div')
+        div.setAttribute("id",`brick-${i}`)
+        if (i%3 === 2){
+            div.dataset.foundation = true
+        }
+        document.body.append(div)
+        if (i === numOfBric) {
+            clearInterval(timer)
+        }
+        i++
+    }, 100)
+}
+
+function repair(...ids){
+    for(let i = 0; i < ids.length; i++){
+        let divId = document.getElementById(ids[i])
+        if (divId.getAttribute("data-foundation")){
+            divId.dataset.repaired = "in progress"
+        }else{
+            divId.dataset.repaired = "true"
+        }
+    }
+}
+
+function destroy(){
+
+    let allDiv = document.querySelectorAll('div')
+    let i = allDiv.length-1
+    let timer= setInterval(function(){
+        allDiv[i].remove()
+        if (i === 0){
+            clearInterval(timer)
+        }
+        i--
+    }, 100)
+}
+
+export {build, repair, destroy}
--- flow.js
+++ flow.js
@@ -1,3 +1,4 @@
+
 function flow(arr) {
     return function (...args) {
         if (args.length > 1) {
