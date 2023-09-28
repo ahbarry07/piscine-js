b85570e89e9b5512ee0db2f54ef78f715795d8bc
ahbarry <barryahmadou135@gmail.com>
2023-09-28 12:48:21 +0000

    get-them-all.js

--- get-them-all.js
+++ get-them-all.js
@@ -1,11 +1,11 @@
 
 const  getArchitects = () => [document.querySelectorAll('body a'), document.querySelectorAll('body span')]
 
-const getClassical = () => [document.querySelectorAll('a .classical'), document.querySelectorAll('a :not(.classical)')]
+const getClassical = () => [document.querySelectorAll('a.classical'), document.querySelectorAll('a:not(.classical)')]
 
-const getActive = () => [document.querySelectorAll('a .classical .active'), document.querySelectorAll('a .classical :not(.active)')]
+const getActive = () => [document.querySelectorAll('a.classical.active'), document.querySelectorAll('a.classical:not(.active)')]
 
-const getBonannoPisano = () => [document.getElementById('BonannoPisano'), document.querySelectorAll('a .classic .active')]
+const getBonannoPisano = () => [document.getElementById('BonannoPisano'), document.querySelectorAll('a.classical.active')]
 
 
 export {getArchitects, getClassical, getActive, getBonannoPisano}
