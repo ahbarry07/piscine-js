52c0afa70af0ae3236c0e8376ce880c855c7786b
ahbarry <barryahmadou135@gmail.com>
2023-09-28 00:03:15 +0000

    flow.js

--- flow.js
+++ flow.js
@@ -1,24 +1,10 @@
 function flow(funcs) {
     return function (...args) {
-      let result = args;
+      let result = [args]; // Toujours traiter le résultat comme un tableau
       for (const func of funcs) {
-        if (Array.isArray(result)) {
-            console.log("hi")
-          result = result.map((item) => func(item));
-        } else {
-          console.log("hello")
-          result = func(result);
-        }
-        // if (Math.floor(result) === -7) return 0;
-        // if (Math.floor(result) === -24) return -18;
-        // if (Math.floor(result) === -12) return 4;
-        // if (Math.floor(result) === -24) return -18;
-        // if (Math.floor(result) === 8) return 15;
-
+        result = result.map((arr) => func(...arr));
       }
-      if (Array.isArray(result)) return result[0]
-      else return result
-    //   return  result;
+      return result[0]; // Renvoyer le premier (et seul) élément du tableau
     };
   }
   
