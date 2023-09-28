3abc6ee07b4065c24b09dfee49041192144dcc4c
ahbarry <barryahmadou135@gmail.com>
2023-09-28 12:42:40 +0000

    get-them-all.js

---
+++ get-them-all.js
@@ -0,0 +1,11 @@
+
+const  getArchitects = () => [document.querySelectorAll('body a'), document.querySelectorAll('body span')]
+
+const getClassical = () => [document.querySelectorAll('a .classical'), document.querySelectorAll('a :not(.classical)')]
+
+const getActive = () => [document.querySelectorAll('a .classical .active'), document.querySelectorAll('a .classical :not(.active)')]
+
+const getBonannoPisano = () => [document.getElementById('BonannoPisano'), document.querySelectorAll('a .classic .active')]
+
+
+export {getArchitects, getClassical, getActive, getBonannoPisano}
