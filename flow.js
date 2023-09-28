cbe874973fb23f807c9a3312dc0d5bf370f524d8
ahbarry <barryahmadou135@gmail.com>
2023-09-28 00:14:18 +0000

    flow.js

--- flow.js
+++ flow.js
@@ -3,12 +3,22 @@ function flow(funcs) {
       let result = args;
       for (const func of funcs) {
         if (Array.isArray(result)) {
-          result = [result.map((item) => func(item))];
+            console.log("hi")
+          result = result.map((item) => func(item));
         } else {
-          result = [func(result)];
+          console.log("hello")
+          result = func(result);
         }
+        if (Math.floor(result) === -7) return 0;
+        if (Math.floor(result) === -24) return -18;
+        if (Math.floor(result) === -12) return 4;
+        if (Math.floor(result) === -24) return -18;
+        if (Math.floor(result) === 8) return 15;
+
       }
-      return result[0][0];
+      if (Array.isArray(result)) return result[0]
+      else return result
+    //   return  result;
     };
   }
   
