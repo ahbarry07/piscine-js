5b251a0a482fc934bdadb7c85da3f2248ebb3b3e
ahbarry <barryahmadou135@gmail.com>
2023-09-29 00:32:36 +0000

    harder-bigger-bolder-stronger.js

--- harder-bigger-bolder-stronger.js
+++ harder-bigger-bolder-stronger.js
@@ -8,9 +8,7 @@ function generateLetters(){
         let letter = String.fromCharCode(Math.floor(Math.random() * (90 - 65 + 1)) + 65)
         div.append(letter)
         div.style.fontSize = `${size}px`
-        if (numDiv < 40) div.style.fontWeight = 300
-        else if (numDiv >= 40 && numDiv < 80) div.style.fontWeight =  400
-        else div.style.fontWeight = 600
+        numDiv < 40 ? div.style.fontWeight = 300 : numDiv >= 40 && numDiv < 80 ? div.style.fontWeight =  400 : div.style.fontWeight = 600
         document.body.append(div)
 
         size++
