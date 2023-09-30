4add19b498ecb5a40e811622e29cd8b62d91918c
ahbarry <barryahmadou135@gmail.com>
2023-09-30 23:37:34 +0000

    mouse-trap.js

--- mouse-trap.js
+++ mouse-trap.js
@@ -1,55 +1,71 @@
-let theLastCircle = null;
-
-function createCircle(){
-
-    document.addEventListener("click",function(e){
-        let xClickPos = e.clientX 
-        let yClickPos = e.clientY 
-        let div = document.createElement('div')
-        div.style.cssText = `left: ${xClickPos}px; top: ${yClickPos}px; background: white;`;
-        div.setAttribute('class', 'circle')
-        document.body.append(div)
-
-        theLastCircle = div
-        console.log(theLastCircle)
+let box
+let flag = true
+let x
+let y
+let circle
+export function createCircle() {
+    addEventListener("click", function () {
+        circle = document.createElement("div")
+        circle.className = "circle"
+        if (flag) {
+            circle.style.background = "white"
+            circle.style.left = x
+            circle.style.top = y
+        } else {
+            circle.style.background = 'var(--purple)'
+            circle.style.left = x
+            circle.style.top = y
+        }
+        document.body.appendChild(circle)
+        flag = true
     })
 }
+export function moveCircle() {
+    addEventListener("mousemove", e => {
+        document.querySelectorAll(".circleRem").forEach((elem) => {
+            elem.remove()
+        })
+        x = e.clientX - 25 + "px"
+        y = e.clientY - 25 + "px"
+        let circle = document.createElement("div")
+        circle.className = "circle"
+        circle.classList.add("circleRem")
+        if (flag) {
+            circle.style.background = "white"
+        } else {
+            circle.style.background = 'var(--purple)'
+        }
+        circle.style.left = e.clientX - 25 + "px"
+        circle.style.top = e.clientY - 25 + "px"
 
-function moveCircle(){
-
-    document.addEventListener('mousemove',(e) =>{
-       if (theLastCircle){
-            let xMousePos = e.clientX 
-            let yMousePos = e.clientY 
-            theLastCircle.style.cssText =`left: ${xMousePos}px; top: ${yMousePos}px; background: white;` 
+        document.body.appendChild(circle)
+        if ((e.clientX >= box.getBoundingClientRect().left + 25 && e.clientX <= box.getBoundingClientRect().right - 25) && (e.clientY >= box.getBoundingClientRect().top + 25 && e.clientY <= box.getBoundingClientRect().bottom - 25)) {
+            document.querySelector(".circle").style.background = 'var(--purple)'
+            flag = false
+        }
+        if (!flag) {
+            if (e.clientX - 25 < box.getBoundingClientRect().left) {
+                circle.style.left = box.getBoundingClientRect().left + "px"
+                document.querySelector(".circle").style.background = 'var(--purple)'
+            }
+            if (e.clientX + 25 > box.getBoundingClientRect().right) {
+                circle.style.left = box.getBoundingClientRect().right - 50 + "px"
+                document.querySelector(".circle").style.background = 'var(--purple)'
+            }
+            if (e.clientY - 25 < box.getBoundingClientRect().top) {
+                circle.style.top = box.getBoundingClientRect().top + "px"
+                document.querySelector(".circle").style.background = 'var(--purple)'
+            }
+            if (e.clientY + 25 > box.getBoundingClientRect().bottom) {
+                circle.style.top = box.getBoundingClientRect().bottom - 50 + "px"
+                document.querySelector(".circle").style.background = 'var(--purple)'
+            }
         }
     })
 }
-
-function setBox() {
-    let section = document.createElement('section');
-    section.setAttribute('class', 'box');
-    document.body.append(section);
-    let boxCordonnate = section.getBoundingClientRect()
-    let [boxCortop, boxCorbottom, boxCorRight, boxCorleft] = [boxCordonnate.top+1, boxCordonnate.bottom, boxCordonnate.right-1, boxCordonnate.left+1]
-   
-        window.addEventListener('mousemove', () => {
-            if (theLastCircle){
-                let lastCircleCoordonnate = theLastCircle.getBoundingClientRect()
-                let [lastCircleTop, lastCircleBottom, lastCircleRight, lastCircleLeft] = [lastCircleCoordonnate.top, lastCircleCoordonnate.bottom, lastCircleCoordonnate.right, lastCircleCoordonnate.left]
-                if (lastCircleTop >= boxCortop && lastCircleBottom <= boxCorbottom && lastCircleRight <= boxCorRight && lastCircleLeft >= boxCorleft){
-                    console.log('cercle:', lastCircleTop, lastCircleBottom, lastCircleRight, lastCircleLeft)
-                    console.log('box:', boxCortop, boxCorbottom, boxCorRight, boxCorleft)
-                    theLastCircle.style.backgroundColor = 'var(--purple)'
-                    section.style.position = 'relative'
-                    section.style.overflow ='hidden'
-                    theLastCircle.style.position = 'absolute'
-                    theLastCircle.style.pointerEvents = 'none'
-
-                }
-            }
-        })
-    
+export function setBox() {
+    box = document.createElement("div")
+    box.className = "box"
+    document.body.appendChild(box)
+    console.log(box.getBoundingClientRect().bottom)
 }
-
-export {createCircle, moveCircle, setBox}
