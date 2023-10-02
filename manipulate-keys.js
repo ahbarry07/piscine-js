da27458a1b93b4c53d02fa233668e054730212c4
ahbarry <barryahmadou135@gmail.com>
2023-10-02 23:16:18 +0000

    manipulate-keys.js

--- manipulate-keys.js
+++ manipulate-keys.js
@@ -26,21 +26,3 @@ function reduceKeys(obj, reducer, initialValue) {
     }
     return accumulator;
 }
-  
-const nutrients = {
-    'vinegar': 80,
-    'sugar': 100,
-    'oil': 50,
-    'onion': 200,
-    'garlic': 22,
-    'paprika': 4,
-  }
-
-console.log(filterKeys(nutrients, (key) => /protein/.test(key)))
-// output: { protein: 20 }
-
-console.log(mapKeys(nutrients, (k) => `-${k}`))
-// output: { -carbohydrates: 12, -protein: 20, -fat: 5 }
-
-console.log(reduceKeys(nutrients, (acc, cr) => `${acc}${cr}:`, ':'))
-// output: carbohydrates, protein, fat
