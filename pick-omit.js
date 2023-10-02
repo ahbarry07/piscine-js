c0e61a88ac06a818c7c663a06e1b2618b84b4804
ahbarry <barryahmadou135@gmail.com>
2023-10-02 15:19:36 +0000

    pick-omit.js

--- pick-omit.js
+++ pick-omit.js
@@ -13,7 +13,7 @@ function pick(obj, str_or_arr){
 
 function omit(obj, str_or_arr){
     let newObj = {}
-    if (!(Array.isArray(str_or_arr))) str_or_arr = str_or_arr.join(' ')
+    if ((Array.isArray(str_or_arr))) str_or_arr = str_or_arr.join(' ')
     for(let [key, value] of Object.entries(obj)){
         if(!(str_or_arr.includes(key))){
             newObj[key] = value
