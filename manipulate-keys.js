ded824413af9d08db379364ebb065a6541c096f9
ahbarry <barryahmadou135@gmail.com>
2023-10-02 22:01:54 +0000

    manipulate-keys.js

---
+++ manipulate-keys.js
@@ -0,0 +1,28 @@
+function filterKeys(obj, args){
+    let resultObj = {}
+    for(let key in obj){
+        if (args(key, obj[key], obj)){
+            resultObj[key] = obj[key]
+        }
+    }
+    return resultObj
+}
+
+function mapKeys(obj, args){
+    let resultObj = {}
+    for(let key in obj){
+        const newKey = args(key, obj[key], obj)
+        resultObj[newKey] = obj[key] 
+    }
+    return resultObj
+}
+
+function reduceKeys(obj, args, add=''){
+    let acc = Object.keys(obj)[0]
+    for(let key in obj){
+        acc = args(acc, key, obj[key], obj)
+    }
+    return acc.split(', ').slice(1).join(', ') + add
+   
+}
+
