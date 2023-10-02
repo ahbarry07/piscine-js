198aaf6a42310e57de28a379bd6495039456c4fd
ahbarry <barryahmadou135@gmail.com>
2023-10-02 21:05:01 +0000

    manipulate-values.js

---
+++ manipulate-values.js
@@ -0,0 +1,28 @@
+
+function filterValues(obj, args){
+    let resultObj = {}
+    for (let key in obj){
+        if(args(obj[key], key, obj)){
+            resultObj[key] = obj[key]
+        } 
+    }
+    return resultObj
+}
+
+function mapValues(obj, args){
+    let resultObj = {}
+    for (let key in obj){
+        if(args(obj[key], key, obj)){
+            resultObj[key] = obj[key] + 1
+        } 
+    }
+    return resultObj
+}
+
+function reduceValues(obj, args){
+    let acc = Object.values(obj)[0]
+    for(let key in obj){
+        acc = args(acc, obj[key], key, obj) 
+    }
+    return acc - Object.values(obj)[0]
+}
