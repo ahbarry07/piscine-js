b13e7565443a40dd5ce33dbfb256ef4983007185
ahbarry <barryahmadou135@gmail.com>
2023-10-02 17:37:08 +0000

    fusion.js

--- fusion.js
+++ fusion.js
@@ -9,8 +9,9 @@ function fusion(obj1, obj2){
             typeof obj1[key] === "number" && typeof obj2[key] === "number" ?  objConcat[key] = obj1[key] + obj2[key]       :     
             typeof obj1[key] === "object" && typeof obj2[key] === "object" ?  objConcat[key] = fusion(obj1[key], obj2[key]):
                                                                               objConcat[key] = obj2[key]
-        }else if (obj1.hasOwnProperty(key)) objConcat[key] = obj1[key]
-         else if (obj2.hasOwnProperty(key)) objConcat[key] = obj2[key]
+        }
+        else if (obj1.hasOwnProperty(key)) objConcat[key] = obj1[key]
+        else if (obj2.hasOwnProperty(key)) objConcat[key] = obj2[key]
     }
     for (const clé in obj2) {
         if (!obj1.hasOwnProperty(clé)) objConcat[clé] = obj2[clé]    
