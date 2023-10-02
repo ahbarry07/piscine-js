eaf4c0c93b62289710fb495fdf5b34456ba9dd0b
ahbarry <barryahmadou135@gmail.com>
2023-10-02 14:55:58 +0000

    pick-omit.js

---
+++ pick-omit.js
@@ -0,0 +1,23 @@
+
+function pick(obj, str_or_arr){
+    let newObj = {}
+    if (!(Array.isArray(str_or_arr))) str_or_arr = str_or_arr.split(' ')
+
+    for(let i in str_or_arr){
+        if(obj.hasOwnProperty(str_or_arr[i])){
+            newObj[str_or_arr[i]] = obj[str_or_arr[i]]
+        }
+    }
+    return newObj
+}
+
+function omit(obj, str_or_arr){
+    let newObj = {}
+    if (!(Array.isArray(str_or_arr))) str_or_arr = str_or_arr.split(' ')
+    for(let i in str_or_arr){
+        if(!(obj.hasOwnProperty(str_or_arr[i]))){
+            newObj[str_or_arr[i]] = obj[str_or_arr[i]]
+        }
+    }
+    return newObj
+}
