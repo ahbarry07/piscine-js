f7e8e677e770d1cd664382527bbeaaa79a98b551
ahbarry <barryahmadou135@gmail.com>
2023-10-03 15:17:53 +0000

    fixed manipulatio.entries

--- manipulate-entries.js
+++ manipulate-entries.js
@@ -11,7 +11,7 @@ function filterEntries(obj, args) {
 
 const mapEntries = (obj, fn) =>
   Object.fromEntries(
-    Object.entries(obj).map(([key, value]) => fn([key, value])) 
+    Object.entries(obj).map(([key, value]) => fn([key, value]))
   );
 
 const reduceEntries = (obj, args, initialValue) =>
@@ -41,78 +41,14 @@ const lowCarbs = (obj) =>
     (curr) => (nutritionDB[curr[0]]["carbs"] / 100) * curr[1] < 50
   );
 
-const cartTotal = (obj) =>
-  mapEntries(obj, (curr) => {
-    let newObj = {};
-    for (let [key, value] of Object.entries(nutritionDB[curr[0]])) {
-      newObj[key] = parseFloat(
-        ((nutritionDB[curr[0]][key] / 100) * curr[1]).toFixed(1)
-      );
-    }
-    return newObj;
-  });
-// const nutritionDB = {
-//   tomato: {
-//     calories: 18,
-//     protein: 0.9,
-//     carbs: 3.9,
-//     sugar: 2.6,
-//     fiber: 1.2,
-//     fat: 0.2,
-//   },
-//   vinegar: {
-//     calories: 20,
-//     protein: 0.04,
-//     carbs: 0.6,
-//     sugar: 0.4,
-//     fiber: 0,
-//     fat: 0,
-//   },
-//   oil: { calories: 48, protein: 0, carbs: 0, sugar: 123, fiber: 0, fat: 151 },
-//   onion: { calories: 0, protein: 1, carbs: 9, sugar: 0, fiber: 0, fat: 0 },
-//   garlic: {
-//     calories: 149,
-//     protein: 6.4,
-//     carbs: 33,
-//     sugar: 1,
-//     fiber: 2.1,
-//     fat: 0.5,
-//   },
-//   paprika: {
-//     calories: 282,
-//     protein: 14.14,
-//     carbs: 53.99,
-//     sugar: 1,
-//     fiber: 0,
-//     fat: 12.89,
-//   },
-//   sugar: {
-//     calories: 387,
-//     protein: 0,
-//     carbs: 100,
-//     sugar: 100,
-//     fiber: 0,
-//     fat: 0,
-//   },
-//   orange: {
-//     calories: 49,
-//     protein: 0.9,
-//     carbs: 13,
-//     sugar: 9,
-//     fiber: 0.2,
-//     fat: 0.1,
-//   },
-// };
-// const groceriesCart1 = { oil: 500, onion: 230, garlic: 220, paprika: 480 };
-// // console.log(
-// //   reduceEntries(groceriesCart1, (acc, [k, v]) => acc + k + v, "") ===
-// //     "oil500onion230garlic220paprika480"
-// // );
+const cartTotal = (obj) => {
+  let newObj = {};
 
-// console.log(
-//   mapEntries(
-//     filterEntries(groceriesCart1, ([k, v]) => k === "onion"),
-//     ([k, v]) => [`✔️${k}`, v - 100]
-//   ),
-//   { "✔️onion": 130 }
-// );
+  for (const [key, value] of Object.entries(obj)) {
+    newObj[key] = mapEntries(nutritionDB[key], ([k, v]) => [
+      k,
+      parseFloat(((v / 100) * value).toFixed(1)),
+    ]);
+  }
+  return newObj;
+};
