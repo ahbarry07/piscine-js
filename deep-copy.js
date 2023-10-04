e175a7d47ec750a5d183ead81bd530f82f441005
ahbarry <barryahmadou135@gmail.com>
2023-10-04 15:58:20 +0000

    deepcopy.js

--- deep-copy.js
+++ deep-copy.js
@@ -9,11 +9,9 @@ function deepCopy(obj_or_array){
         }
         return newArray
     }
-    if (typeof obj_or_array==='object'){
-        let newObj = {}
-        for(let [key, value] of obj_or_array){
-            newObj[key] = deepCopy(value)
-        }
-        return newObj
+    let newObj = {}
+    for(let key in obj_or_array){
+        newObj[key] = deepCopy(obj_or_array[key])
     }
+    return newObj
 }
