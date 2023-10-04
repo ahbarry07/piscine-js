059928f26f9774a1cfe8168f7e687df4630f87f9
ahbarry <barryahmadou135@gmail.com>
2023-10-04 22:13:45 +0000

    replica.js

--- replica.js
+++ replica.js
@@ -1,18 +1,23 @@
+let is = {}
+is.obj = function (objet){
+    if (Array.isArray(objet) || objet === null || objet instanceof RegExp){
+        return false
+    }
+    return (typeof objet) === "object"  
+}
 
 function replica(obj, ...target){
 
     for(const src of target){
         for (const key in src){
-            if ((!Array.isArray(obj[key])) && obj[key] !== null && !obj[key] instanceof RegExp){
-                if (!obj.hasOwnProperty(key) || (!Array.isArray(obj[key])) && obj[key] !== null && obj[key] instanceof !RegExp){
-                    obj[key] = {}
+            if (is.obj(src[key])){
+                if (obj.hasOwnProperty(key) && is.obj(obj[key])){
+                    replica(obj[key], src[key])
                 }
-                replica(obj[key], src[key])
             }else{
                 obj[key] = src[key]
             }
         }
     }
-
     return obj
 }
