ff919f05e7e8a8e136e790adad21f404f53934f5
ahbarry <barryahmadou135@gmail.com>
2023-10-04 22:04:17 +0000

    replica.js

--- replica.js
+++ replica.js
@@ -1,5 +1,18 @@
 
 function replica(obj, ...target){
 
-   
+    for(const src of target){
+        for (const key in src){
+            if ((!Array.isArray(obj[key])) && obj[key] !== null && !obj[key] instanceof RegExp){
+                if (!obj.hasOwnProperty(key) || (!Array.isArray(obj[key])) && obj[key] !== null && obj[key] instanceof !RegExp){
+                    obj[key] = {}
+                }
+                replica(obj[key], src[key])
+            }else{
+                obj[key] = src[key]
+            }
+        }
+    }
+
+    return obj
 }
