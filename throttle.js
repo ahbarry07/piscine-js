88770550a09cec491bd4c2b3178359e6637ba85a
ahbarry <barryahmadou135@gmail.com>
2023-10-05 16:57:32 +0000

    throttle.js

--- throttle.js
+++ throttle.js
@@ -12,27 +12,25 @@ function throttle(callback, delay) {
 }
 
 function opThrottle(callback, delay, { leading = true, trailing = true } = {}) {
-	let lastTimeOfExecution = 0;
-	let timer;
-	let pendingArgs;
+	let lastTimeOfExecution = 0
+	let timer
 
 	return (...args) => {
-		const timeNow = new Date().getTime();
+		const timeNow = new Date().getTime()
 
-		if (leading && (timeNow - lastTimeOfExecution >= delay || lastTimeOfExecution === 0)) {
-			callback(...args);
-			lastTimeOfExecution = timeNow;
-			leading = false;
-		} else {
-			clearTimeout(timer);
-			pendingArgs = args;
-			timer = setTimeout(() => {
-				if (trailing && pendingArgs) {
-					callback(...pendingArgs);
-					lastTimeOfExecution = new Date().getTime();
-					pendingArgs = null;
-				}
-			}, delay);
+		if (leading) {
+			lastTimeOfExecution = timeNow
+			leading = false
 		}
-	};
+		if (timeNow - lastTimeOfExecution >= delay){
+			callback(...args)
+			lastTimeOfExecution = timeNow
+		}
+		if (trailing) {
+			clearTimeout(timer)
+			timer = setTimeout(() => {
+				callback(...args)
+			}, delay)
+		}
+	}
 }
