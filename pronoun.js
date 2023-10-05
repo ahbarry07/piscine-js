a7b9f64594fcb0280679d3b29e732d7340580aae
ahbarry <barryahmadou135@gmail.com>
2023-10-05 01:51:20 +0000

    pronoun.js

--- pronoun.js
+++ pronoun.js
@@ -6,15 +6,27 @@ const regexp5 = /^it$/;
 const regexp6 = /^we$/;
 const regexp7 = /^they$/;
 
+function findNextWord(word) {
+    const pronouns = /^(i|you|he|she|it|they|we)$/i;
+    if (pronouns.test(word)) {
+        return ;
+    } else {
+        return word.replace(/,/, "");
+    }
+    
+}
+
 function pronoun(str) {
-    let [obj, count1, count2,count3,count4,count5,count6, count7, word1, word2, word3, word4, word5, word6, word7] = [{}, 0,0,0,0,0,0,0,[],[],[],[],[],[],[]]
+    let [obj, count1, count2, count3, count4, count5, count6, count7, word1, word2, word3, word4, word5, word6, word7] = [{}, 0, 0, 0, 0, 0, 0, 0, [], [], [], [], [], [], []]
     const split = str.split("\n").join(' ').split(' ');
-    // console.log(split)
     for (let i = 0; i < split.length; i++) {
         if (regexp1.test(split[i].toLowerCase().toLowerCase())) {
             obj[split[i].toLowerCase()] = {};
             if (split[i].toLowerCase() !== undefined) {
-                word1.push(split[i+1].replace(',', ''))
+                let check = findNextWord(split[i+1].toLowerCase())
+                if (check !== undefined){
+                    word1.push(check)
+                }
                 obj[split[i].toLowerCase()].word = word1
                 count1++;
                 obj[split[i].toLowerCase()].count = count1;
@@ -22,7 +34,10 @@ function pronoun(str) {
         } else if (regexp2.test(split[i].toLowerCase().toLowerCase())) {
             obj[split[i].toLowerCase()] = {};
             if (split[i].toLowerCase() !== undefined) {
-                word2.push(split[i+1].replace(',', ''))
+                let check = findNextWord(split[i+1].toLowerCase())
+                if (check !== undefined){
+                    word2.push(check)
+                }
                 obj[split[i].toLowerCase()].word = word2
                 count2++;
                 obj[split[i].toLowerCase()].count = count2;
@@ -30,7 +45,10 @@ function pronoun(str) {
         } else if (regexp3.test(split[i].toLowerCase().toLowerCase())) {
             obj[split[i].toLowerCase()] = {};
             if (split[i].toLowerCase() !== undefined) {
-                word3.push(split[i+1].replace(',', ''))
+                let check = findNextWord(split[i+1].toLowerCase())
+                if (check !== undefined){  
+                    word3.push(check)
+                }
                 obj[split[i].toLowerCase()].word = word3
                 count3++;
                 obj[split[i].toLowerCase()].count = count3;
@@ -38,15 +56,21 @@ function pronoun(str) {
         } else if (regexp4.test(split[i].toLowerCase().toLowerCase())) {
             obj[split[i].toLowerCase()] = {};
             if (split[i].toLowerCase() !== undefined) {
-                word4.push(split[i+1].replace(',', ''))
+                let check = findNextWord(split[i+1].toLowerCase())
+                if (check !== undefined){
+                    word4.push(check)
+                }
                 obj[split[i].toLowerCase()].word = word4
                 count4++;
                 obj[split[i].toLowerCase()].count = count4;
             }
         } else if (regexp5.test(split[i].toLowerCase().toLowerCase())) {
             obj[split[i].toLowerCase()] = {};
-            if (split[i+1] !== undefined) {
-                word5.push(split[i+1].replace(',', ''))
+            if (split[i + 1] !== undefined) {
+                let check = findNextWord(split[i+1].toLowerCase())
+                if (check !== undefined){
+                    word5.push(check)
+                }
                 obj[split[i].toLowerCase()].word = word5
                 count5++;
                 obj[split[i].toLowerCase()].count = count5;
@@ -54,7 +78,10 @@ function pronoun(str) {
         } else if (regexp6.test(split[i].toLowerCase().toLowerCase())) {
             obj[split[i].toLowerCase()] = {};
             if (split[i].toLowerCase() !== undefined) {
-                word6.push(split[i+1].replace(',', ''))
+                let check = findNextWord(split[i+1].toLowerCase())
+                if (check !== undefined){
+                    word6.push(check)     
+                }
                 obj[split[i].toLowerCase()].word = word6
                 count6++;
                 obj[split[i].toLowerCase()].count = count6;
@@ -62,13 +89,16 @@ function pronoun(str) {
         } else if (regexp7.test(split[i].toLowerCase().toLowerCase())) {
             obj[split[i].toLowerCase()] = {};
             if (split[i].toLowerCase() !== undefined) {
-                word7.push(split[i+1].replace(',', ''))
+                let check = findNextWord(split[i+1].toLowerCase())
+                if (check !== undefined){
+                    word7.push(check)
+                }
                 obj[split[i].toLowerCase()].word = word7
                 count7++;
                 obj[split[i].toLowerCase()].count = count7;
             }
         }
-        
+
     }
     return obj
 }
