66b2bf3fd375c29a4d2e674b5ab3cce62428ddb7
ahbarry <barryahmadou135@gmail.com>
2023-10-05 02:08:23 +0000

    neuron.js

--- neuron.js
+++ neuron.js
@@ -1,27 +1,37 @@
-function neuron(data) {
-    
-    const result = {};
+function neuron(arr) {
+    const result = { questions: {}, orders: {}, affirmations: {} };
   
-    for (const item of data) {
-     
-      const [questionPart, responsePart] = item.split(' - Response: ');
-      const [category, label] = questionPart.split(': ');
+    for (let i = 0; i < arr.length; i++) {
+      const parts = arr[i].split(' - ');
+      const category = parts[0].toLowerCase();
+      const [statement, response] = parseStatementResponse(parts[1]);
+
+      const categoryKey = category.replace(':', '').trim();
   
-      const normalizedLabel = label.toLowerCase().replace(/\s+/g, '_');
+      const statementKey = statement.replace('?', '').replace('!', '').trim().toLowerCase();
   
-      const questionOrOrderData = {
-        [category.toLowerCase()]: category,
-        [normalizedLabel]: {
-          [category.toLowerCase()]: label,
-          responses: [responsePart.trim()]
+      if (category.startsWith('questions:')) {
+        if (!result.questions[statementKey]) {
+          result.questions[statementKey] = { question: statement, responses: [] };
         }
-      };
-      if (!result[category.toLowerCase()]) {
-        result[category.toLowerCase()] = {};
+        result.questions[statementKey].responses.push(response);
+      } else if (category.startsWith('orders:')) {
+        if (!result.orders[statementKey]) {
+          result.orders[statementKey] = { order: statement, responses: [] };
+        }
+        result.orders[statementKey].responses.push(response);
+      } else if (category.startsWith('affirmations:')) {
+        if (!result.affirmations[statementKey]) {
+          result.affirmations[statementKey] = { affirmation: statement, responses: [] };
+        }
+        result.affirmations[statementKey].responses.push(response);
       }
-  
-      Object.assign(result[category.toLowerCase()], questionOrOrderData);
     }
   
     return result;
-}
+  }
+  
+  function parseStatementResponse(str) {
+    const [statement, response] = str.split(' - ');
+    return [statement.trim(), response.trim()];
+  }
