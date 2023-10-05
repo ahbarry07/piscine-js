ea5b59bec3c0ac86a5d4ffc4330bfce273217c00
ahbarry <barryahmadou135@gmail.com>
2023-10-05 00:51:58 +0000

    pronoun.js

--- pronoun.js
+++ pronoun.js
@@ -7,28 +7,68 @@ const regexp6 = /^we$/;
 const regexp7 = /^they$/;
 
 function pronoun(str) {
-    const word = str.split(" ");
-    let obj = {}
-    let count = 0
-    for (let i = 0; i < word.length; i++) {
-        if (
-            regexp1.test(word[i]) ||
-            regexp2.test(word[i]) ||
-            regexp3.test(word[i]) ||
-            regexp4.test(word[i]) ||
-            regexp5.test(word[i]) ||
-            regexp6.test(word[i]) ||
-            regexp7.test(word[i])
-        ) {
-            obj[word[i]] = {}
-            if (word[i] !== undefined) {
-                obj[word[i]].word = []
-                obj[word[i]].word.push(word[i + 1])
-                count++;
-                obj[word[i]].count = count
+    let [obj, count1, count2,count3,count4,count5,count6, count7, word1, word2, word3, word4, word5, word6, word7] = [{}, 0,0,0,0,0,0,0,[],[],[],[],[],[],[]]
+    const split = str.split(" ");
+    for (let i = 0; i < split.length; i++) {
+        if (regexp1.test(split[i])) {
+            obj[split[i]] = {};
+            if (split[i] !== undefined) {
+                word1.push(split[i+1])
+                obj[split[i]].word = word1
+                count1++;
+                obj[split[i]].count = count1;
+            }
+        } else if (regexp2.test(split[i])) {
+            obj[split[i]] = {};
+            if (split[i] !== undefined) {
+                word2.push(split[i])
+                obj[split[i]].word = word2
+                count2++;
+                obj[split[i]].count = count2;
+            }
+        } else if (regexp3.test(split[i])) {
+            obj[split[i]] = {};
+            
+            if (split[i] !== undefined) {
+                word3.push(split[i+1])
+                obj[split[i]].word = word3
+                count3++;
+                obj[split[i]].count = count3;
+            }
+        } else if (regexp4.test(split[i])) {
+            obj[split[i]] = {};
+            if (split[i] !== undefined) {
+                word4.push(split[i+1])
+                obj[split[i]].word = word4
+                count4++;
+                obj[split[i]].count = count4;
+            }
+        } else if (regexp5.test(split[i])) {
+            obj[split[i]] = {};
+            if (split[i+1] !== undefined) {
+                word5.push(split[i+1])
+                obj[split[i]].word = word5
+                count5++;
+                obj[split[i]].count = count5;
+            }
+        } else if (regexp6.test(split[i])) {
+            obj[split[i]] = {};
+            if (split[i] !== undefined) {
+                word6.push(split[i+1])
+                obj[split[i]].word = word6
+                count6++;
+                obj[split[i]].count = count6;
+            }
+        } else if (regexp7.test(split[i])) {
+            obj[split[i]] = {};
+            if (split[i] !== undefined) {
+                word7.push(split[i+1])
+                obj[split[i]].word = word7
+                count7++;
+                obj[split[i]].count = count7;
             }
         }
+        
     }
     return obj
 }
-
