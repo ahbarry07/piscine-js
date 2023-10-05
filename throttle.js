fcd5264a90d63cfb53e8baceb9c11eb2ad79399e
ahbarry <barryahmadou135@gmail.com>
2023-10-05 14:02:36 +0000

    throttle.js

---
+++ throttle.js
@@ -0,0 +1,29 @@
+
+function throttle(callback, delay){
+	let lastTime = 0
+	return (...args) => {
+		const timeNow = new Date().getTime()
+		if (timeNow - lastTime < delay){
+			return
+		}
+		lastTime = timeNow
+		return callback(...args)
+	}
+}
+
+function opThrottle(callback, delay, lead = false){
+	let inc = true
+	const timeNow = new Date().getTime()
+	return (...args) =>{
+		if (inc && lead){
+			callback(...args)
+		}
+		inc = false
+
+		if (timeNow - lastTime < delay){
+			return
+		}
+		lastTime = timeNow
+		return callback(...args)
+	}
+}
