f4e210ab4d7a0f4c1d8ae93ed0e8b97f8984d1a1
ahbarry <barryahmadou135@gmail.com>
2023-10-05 02:21:23 +0000

    neuron.js

--- neuron.js
+++ neuron.js
@@ -1,40 +1,44 @@
-function neuron(data) {
-   
-    const mutatedData = {
-      questions: {},
-      orders: {}
-    };
-  
-    for (const line of data) {
-      
-      const [questionOrOrder, response] = line.split(": ");
-  
-      if (questionOrOrder.startsWith("Questions: ")) {
-        const question = questionOrOrder.substring("Questions: ".length);
-  
-        if (mutatedData.questions[question]) {
-          mutatedData.questions[question].responses.push(response);
-        } else {
-          mutatedData.questions[question] = {
-            question,
-            responses: [response]
-          };
+function neuron(arr) {
+    var res = {};
+    for (let i = 0; i < arr.length; i++) {
+        let str = arr[i].split(' ');
+        if (/questions:/i.test(str[0])) {
+            res['questions'] ||= {};
+            let [question, response] = parseQuestion(str);
+            let questionKey = question.replaceAll(' ', '_').replace('?', '').toLowerCase();
+            res['questions'][questionKey] ||= {};
+            res['questions'][questionKey]['question'] = question;
+            res['questions'][questionKey]['responses'] ||= [];
+            res['questions'][questionKey]['responses'].push(response);
+        } else if (/orders:/i.test(str[0])) {
+            let [order, response] = parseQuestion(str);
+            res['orders'] ||= {};
+            let orderKey = order.replaceAll(' ', '_').replace('!', '').toLowerCase();
+            res['orders'][orderKey] ||= {};
+            res['orders'][orderKey]['order'] = order;
+            res['orders'][orderKey]['responses'] ||= [];
+            res['orders'][orderKey]['responses'].push(response);
+        } else if (/affirmations:/i.test(str[0])) {
+            let [affirmation, response] = parseAffirmations(str);
+            res['affirmations'] ||= {};
+            let affirmationKey = affirmation.replaceAll(' ', '_').toLowerCase();
+            res['affirmations'][affirmationKey] ||= {};
+            res['affirmations'][affirmationKey]['affirmation'] = affirmation;
+            res['affirmations'][affirmationKey]['responses'] ||= [];
+            res['affirmations'][affirmationKey]['responses'].push(response);
         }
-      }
-  
-      else if (questionOrOrder.startsWith("Orders: ")) {
-        const order = questionOrOrder.substring("Orders: ".length);
-  
-        if (mutatedData.orders[order]) {
-          mutatedData.orders[order].responses.push(response);
-        } else {
-          mutatedData.orders[order] = {
-            order,
-            responses: [response]
-          };
-        }
-      }
     }
-  
-    return mutatedData;
-  }
+    return res;
+}
+
+function parseQuestion(arr) {
+    let statement = arr.slice(1).join(' ').split('-')[0].slice(0, -1);
+    let response = arr.join(' ').split('-').slice(1).join('-').slice(1).split(' ').slice(1).join(' ');
+    return [statement, response];
+}
+
+function parseAffirmations(arr) {
+    let statement = arr.slice(1).join(' ').split('-')[0].slice(0, -1);
+    let response = arr.join(' ').split('-')[1].slice(1).split(' ').slice(1) .join(' ');
+    return [statement, response];
+}
