a0add951d62340d6d4450297142a0ba87cdd0ca4
ahbarry <barryahmadou135@gmail.com>
2023-10-09 15:41:23 +0000

    verydisco-reverso.mjs

---
+++ verydisco-reverso.mjs
@@ -0,0 +1,40 @@
+
+import { argv } from 'node:process'
+import { readFile } from 'fs/promises';
+
+let finalWord = []
+const transfrom = (word) => {
+    let newWord = ""
+    let sizing = (Math.round(word.length/2))
+    for (let i = sizing; i < word.length; i++) {
+        newWord += word[i]
+    }
+    for (let j = 0; j < sizing; j++) {
+        newWord += word[j]
+    }
+    return newWord
+}
+
+function readAndTransform(filename){
+
+    readFile(filename, 'utf-8', (err, data) =>{
+        if (err) {
+            console.error('Erreur lors de la lecture du fichier')
+            return
+        }
+        return (transfrom(data))
+    })
+}
+
+
+argv.forEach((val, index) => {
+    if (index === 3) {
+        let content = readAndTransform(val)
+        let parts = content.split(' ')
+        parts.forEach((value) => {
+            finalWord.push(transfrom(value))
+        })
+
+        console.log(finalWord.join(' '))
+    }
+})
