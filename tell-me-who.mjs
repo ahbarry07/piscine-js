26545dd8cc98a042b148fee7ead23b70e47fb439
ahbarry <barryahmadou135@gmail.com>
2023-10-09 21:49:47 +0000

    tell-me-who.mjs

--- tell-me-who.mjs
+++ tell-me-who.mjs
@@ -2,10 +2,20 @@
 import {readdir} from 'fs/promises'
 import { argv } from 'process';
 
+function sortName(a, b) {
+    // Divisez chaque chaîne en mots
+    const prenomA = a.split(" ")[0];
+    const prenomB = b.split(" ")[0];
+  
+    // Comparez les prénoms de manière insensible à la casse
+    return prenomA.localeCompare(prenomB, undefined, { sensitivity: 'base' });
+  }
+
+
 async function tellMeWho(dirPath){
 
     let array = []
-    const content = readdir(dirPath)
+    const content = readdir(dirPath, 'utf-8')
     content.then((data) =>{
         data.forEach(file => {
             const split1 = file.split('.')
@@ -14,7 +24,7 @@ async function tellMeWho(dirPath){
             const name =  `${split2[0]} ${split2[1]}`
             array.push(name)
         });
-        array.sort()
+        array.sort(sortName)
     
         array.forEach((value, index) => {
             console.log(`${index+1}. ${value}`)
