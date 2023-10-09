7a09476c5cd937d70a54423619df95695fbf7643
ahbarry <barryahmadou135@gmail.com>
2023-10-09 18:22:59 +0000

    tell-me-how-many.mjs

---
+++ tell-me-how-many.mjs
@@ -0,0 +1,19 @@
+import { argv, argv0 } from 'node:process'
+import {readdir} from 'fs/promises'
+
+function tellMeHowMany(path){
+
+    let contennt = readdir(path, 'utf-8')
+    return contennt.length
+}
+
+if (argv.length === 0){
+    const currentRepetory = __dirname
+    console.log(tellMeHowMany(currentRepetory))
+}else{
+    argv.forEach(index, val => {
+        if (index === 2){
+            console.log(tellMeHowMany(val))
+        }
+    });
+}
