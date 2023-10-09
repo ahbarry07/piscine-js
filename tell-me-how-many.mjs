6b896dcfdcce9981b9f8286437877f4a7531af49
ahbarry <barryahmadou135@gmail.com>
2023-10-09 18:40:28 +0000

    tell-me-how-many.mjs

--- tell-me-how-many.mjs
+++ tell-me-how-many.mjs
@@ -1,24 +1,13 @@
 import { argv, argv0 } from 'node:process'
 import {readdir} from 'fs/promises'
 
-function tellMeHowMany(path){
+async function tellMeHowMany(path){
 
-    let content = readdir(path, 'utf-8')
-    console.log(content)
-    return 9
+    let content = await readdir(path)
+   
+   console.log(content.length)
 }
 
-if (argv.length === 0){
-    const currentRepetory = __dirname
-    const result = tellMeHowMany(currentRepetory)
-    console.log(result)
-    console.log(1)
-}else{
-    argv.forEach((val, index) => {
-        if (index === 2){
-            let result = tellMeHowMany(val)
-            console.log(result)
-            console.log(2)
-        }
-    });
-}
+const directoryPath = process.argv[2] || __dirname
+
+tellMeHowMany(directoryPath)
