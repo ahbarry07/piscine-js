1bf5f4a254c5e5bbfe604fd43d732db00b9410d7
ahbarry <barryahmadou135@gmail.com>
2023-10-09 21:19:03 +0000

    tell-me-who.mjs

--- tell-me-who.mjs
+++ tell-me-who.mjs
@@ -6,15 +6,16 @@ async function tellMeWho(dirPath){
 
     let array = []
     const content = readdir(dirPath)
-   
-    content.forEach(file => {
-        array.push(file)
-    });
-    array.sort()
-
-    array.forEach((value, index) => {
-        console.log(`${index+1}. ${value}`)
-    });
+    content.then((data) =>{
+        data.forEach(file => {
+            array.push(file)
+        });
+        array.sort()
+    
+        array.forEach((value, index) => {
+            console.log(`${index+1}. ${value}`)
+        });
+    })
 }
 
 tellMeWho(process.argv[2])
