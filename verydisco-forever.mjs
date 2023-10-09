334ffa87f64898fbca1f142eed7a3f8fc74474f2
ahbarry <barryahmadou135@gmail.com>
2023-10-09 13:08:07 +0000

    verydisco-forever.mjs

---
+++ verydisco-forever.mjs
@@ -0,0 +1,40 @@
+import { sign } from 'node:crypto';
+import { argv } from 'node:process'
+import { workerData } from 'node:worker_threads';
+const fs = require('fs').promises;
+let contenu 
+
+let finalWord = []
+const transfrom = (word) => {
+    let newWord = ""
+    let sizing = (Math.round(word.length/2))
+    for (let i = sizing; i < word.length; i++) {
+        newWord += word[i]
+    }
+    for (let j = 0; j < sizing; j++) {
+        newWord += word[j]
+    }
+    return newWord
+}
+argv.forEach((val, index) => {
+    if (index === 2) {
+        let parts = val.split(' ')
+        parts.forEach((value) => {
+            finalWord.push(transfrom(value))
+        })
+        contenu = finalWord.join(' ')
+        // console.log(finalWord.join(' '))
+    }
+})
+
+
+async function veryDiscoForever() {
+  try {
+    // Écrire le contenu dans un fichier
+    await fs.writeFile('/piscine-js/verydisco-forever.txt', contenu);
+  } catch (err) {
+    console.error("Erreur lors de l'écriture dans verydisco-forever.txt : ", err);
+  }
+}
+
+veryDiscoForever();
