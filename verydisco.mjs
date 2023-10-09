e3915de65cd11ba7c10af4074e3f017bb6041065
ahbarry <barryahmadou135@gmail.com>
2023-10-09 11:55:04 +0000

    verydisco.mjs

--- verydisco.mjs
+++ verydisco.mjs
@@ -1,23 +1,26 @@
+#!/usr/bin/env node
 
+// Function to make a word "very disco"
 function makeWordVeryDisco(word) {
-    const length = word.length;
-    const middle = Math.ceil(length / 2);
-    const firstHalf = word.slice(0, middle);
-    const secondHalf = word.slice(middle);
-    return secondHalf + firstHalf;
-}
-  
- 
-function processArgument() {
-    const args = process.argv.slice(2);
-    if (args.length === 0) {
-      console.log("Please provide an argument.");
-      return;
-    }
-  
-    const discoWords = args.map(makeWordVeryDisco);
-  
-    console.log(discoWords.join(" "));
+  const length = word.length;
+  const middle = Math.ceil(length / 2);
+  const firstHalf = word.slice(0, middle);
+  const secondHalf = word.slice(middle);
+  return secondHalf + firstHalf;
 }
 
-processArgument()
+// Function to process the command line argument
+function processArgument() {
+  const args = process.argv.slice(2);
+  if (args.length === 0) {
+    console.log("Please provide an argument.");
+    return;
+  }
+
+  const discoWords = args.map(makeWordVeryDisco).join(" ");
+
+  console.log(discoWords);
+}
+
+// Call the function to process the argument
+processArgument();
