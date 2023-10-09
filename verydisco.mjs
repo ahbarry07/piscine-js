a191c67550a69da2a59fec5b4ede04fc554e3615
ahbarry <barryahmadou135@gmail.com>
2023-10-09 11:30:56 +0000

    verydisco.mjs

--- verydisco.mjs
+++ verydisco.mjs
@@ -1,23 +1,16 @@
+import process from 'process';
 
-function makeWordVeryDisco(word) {
-    const length = word.length;
-    const middle = Math.ceil(length / 2);
-    const firstHalf = word.slice(0, middle);
-    const secondHalf = word.slice(middle);
-    return secondHalf + firstHalf;
-}
-  
- 
-function processArgument() {
-    const args = process.argv.slice(2);
-    if (args.length === 0) {
-      console.log("Please provide an argument.");
-      return;
-    }
-  
-    const discoWords = args.map(makeWordVeryDisco);
-  
-    console.log(discoWords.join(" "));
-}
+const argument = process.argv[1];
 
-processArgument()
+export function veryDisco(argument) {
+
+  const words = argument.split(' ');
+  const veryDiscoWords = words.map(word => {
+    const halfLength = Math.ceil(word.length / 2);
+    const firstChunk = word.slice(0, halfLength);
+    const secondChunk = word.slice(halfLength);
+    return secondChunk + firstChunk;
+  });
+
+  return veryDiscoWords.join(' ');
+}
