45004018c57e0deee035f4b1fcb7c53352a45e51
ahbarry <barryahmadou135@gmail.com>
2023-10-09 11:15:35 +0000

    verydisco

---
+++ verydisco.mjs
@@ -0,0 +1,23 @@
+
+function makeWordVeryDisco(word) {
+    const length = word.length;
+    const middle = Math.ceil(length / 2);
+    const firstHalf = word.slice(0, middle);
+    const secondHalf = word.slice(middle);
+    return secondHalf + firstHalf;
+}
+  
+ 
+function processArgument() {
+    const args = process.argv.slice(2);
+    if (args.length === 0) {
+      console.log("Please provide an argument.");
+      return;
+    }
+  
+    const input = args.join(" ");
+    const words = input.split(" ");
+    const discoWords = words.map(makeWordVeryDisco);
+  
+    console.log(discoWords.join(" "));
+}
