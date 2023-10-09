967ee6d1659ad5a01a5f286002868ca470e8fe71
ahbarry <barryahmadou135@gmail.com>
2023-10-09 11:54:25 +0000

    verydisco.mjs

--- verydisco.mjs
+++ verydisco.mjs
@@ -1,27 +1,23 @@
-#!/usr/bin/env node
 
-// Fonction pour rendre un mot "very disco"
 function makeWordVeryDisco(word) {
     const length = word.length;
     const middle = Math.ceil(length / 2);
     const firstHalf = word.slice(0, middle);
     const secondHalf = word.slice(middle);
     return secondHalf + firstHalf;
-  }
+}
   
-  // Fonction pour traiter l'argument de ligne de commande
-  function processArgument() {
+ 
+function processArgument() {
     const args = process.argv.slice(2);
     if (args.length === 0) {
-      console.log("Veuillez fournir un argument.");
+      console.log("Please provide an argument.");
       return;
     }
   
-    const discoWords = args.map(word => makeWordVeryDisco(word)).join(" ");
+    const discoWords = args.map(makeWordVeryDisco);
   
-    console.log(discoWords);
-  }
-  
-  // Appeler la fonction pour traiter l'argument
-  processArgument();
-  
+    console.log(discoWords.join(" "));
+}
+
+processArgument()
