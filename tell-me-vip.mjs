6a67e6a7489afb0c69e4d956c56b8de2b27f7874
ahbarry <barryahmadou135@gmail.com>
2023-10-10 00:36:10 +0000

    tell-me-vip.mjs

--- tell-me-vip.mjs
+++ tell-me-vip.mjs
@@ -1,35 +1,37 @@
-import { readFile, readdir, writeFile } from 'fs/promises';
-
-const regexp = /^YES$/;
-
-async function tellMeVip(dirPath) {
-  try {
-    const fileNames = await readdir(dirPath, 'utf-8');
-    const vipGuests = [];
-
-    for (const fileName of fileNames) {
-      const filePath = `${dirPath}/${fileName}`;
-      const data = await readFile(filePath, 'utf-8');
-
-      if (regexp.test(data.trim())) {
-        const nameParts = fileName.split('_');
-        const firstName = nameParts[1].replace('.json', '');
-        const lastName = nameParts[0];
-        vipGuests.push({ firstName, lastName });
-      }
+import { argv } from 'node:process'
+import { writeFile, readFile, readdir } from 'fs/promises';
+let pathToDirectory = '.'
+let file
+if (argv.length === 2) {
+    file = await readdir(pathToDirectory)
+} else if (argv.length >= 3) {
+    pathToDirectory = argv[2]
+    file = await readdir(pathToDirectory)
+}
+let listAgreed = []
+for (const value of file) {
+    try{
+        let guestInvitation = pathToDirectory + '/' + value
+        let guestAnswer = await readFile(guestInvitation)
+        let toObject = JSON.parse(guestAnswer)
+        if (toObject.answer === 'yes') {
+            listAgreed.push(value)
+        }
+    }catch (e) {
+       
     }
-
-    vipGuests.sort((a, b) => {if ( a[1] < b[1] ){ return -1; } if ( a[1] > b[1] ){ return 1; } return 0; })
-
-
-    const formattedGuests = vipGuests.map((guest, index) => {
-      return `${index + 1}. ${guest.lastName} ${guest.firstName}`;
-    });
-
-    await writeFile('vip.txt', formattedGuests.join('\n'));
-  } catch (error) {
-    console.error('Error:', error);
-  }
 }
 
-tellMeVip(process.argv[2]);
+let listGuest = []
+listAgreed.forEach(async element => {
+    element = element.replace('.json', '').split('_')
+    listGuest.push(element)
+});
+// }
+listGuest.sort((a, b) => {if ( a[1] < b[1] ){ return -1; } if ( a[1] > b[1] ){ return 1; } return 0; })
+let finalList = []
+listGuest.forEach((val, index) => {
+    finalList.push(`${index+1}. ${val[1]} ${val[0]}`)
+})
+
+await writeFile("vip.txt", finalList.join('\n'));
