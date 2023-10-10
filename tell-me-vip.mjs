5a7c7b336d2ebdbb435a099105aafcbe283d18c1
ahbarry <barryahmadou135@gmail.com>
2023-10-10 00:34:20 +0000

    tell-me-vip.mjs

--- tell-me-vip.mjs
+++ tell-me-vip.mjs
@@ -1,32 +1,35 @@
-import { argv } from 'node:process'
-import { writeFile, readFile, readdir } from 'fs/promises';
+import { readFile, readdir, writeFile } from 'fs/promises';
 
-const file = await readdir(process.argv[2])
-let listAgreed = []
-for (const value of file) {
-    try{
-        let guestInvitation = pathToDirectory + '/' + value
-        let guestAnswer = await readFile(guestInvitation)
-        let toObject = JSON.parse(guestAnswer)
-        if (toObject.answer === 'yes') {
-            listAgreed.push(value)
-        }
-    }catch (e) {
-       
+const regexp = /^YES$/;
+
+async function tellMeVip(dirPath) {
+  try {
+    const fileNames = await readdir(dirPath, 'utf-8');
+    const vipGuests = [];
+
+    for (const fileName of fileNames) {
+      const filePath = `${dirPath}/${fileName}`;
+      const data = await readFile(filePath, 'utf-8');
+
+      if (regexp.test(data.trim())) {
+        const nameParts = fileName.split('_');
+        const firstName = nameParts[1].replace('.json', '');
+        const lastName = nameParts[0];
+        vipGuests.push({ firstName, lastName });
+      }
     }
+
+    vipGuests.sort((a, b) => {if ( a[1] < b[1] ){ return -1; } if ( a[1] > b[1] ){ return 1; } return 0; })
+
+
+    const formattedGuests = vipGuests.map((guest, index) => {
+      return `${index + 1}. ${guest.lastName} ${guest.firstName}`;
+    });
+
+    await writeFile('vip.txt', formattedGuests.join('\n'));
+  } catch (error) {
+    console.error('Error:', error);
+  }
 }
 
-let listGuest = []
-
-listAgreed.forEach(async element => {
-    element = element.split('.')[0].split('_')
-    listGuest.push(element)
-});
-
-listGuest.sort((a, b) => {if ( a[1] < b[1] ){ return -1; } if ( a[1] > b[1] ){ return 1; } return 0; })
-let finalList = []
-listGuest.forEach((val, index) => {
-    finalList.push(`${index+1}. ${val[1]} ${val[0]}`)
-})
-
-await writeFile("vip.txt", finalList.join('\n'));
+tellMeVip(process.argv[2]);
