3359e6056c2eb9e1484bfe01e2be474f89f9d5de
ahbarry <barryahmadou135@gmail.com>
2023-10-10 05:21:45 +0000

    personal-shopper.mjs

--- personal-shopper.mjs
+++ personal-shopper.mjs
@@ -1,101 +1,85 @@
-import { readFile, writeFile, unlink } from 'fs/promises';
+import { readFile, writeFile } from 'fs/promises';
 import { argv } from 'process';
 
-// Fonction pour lire le contenu du fichier JSON
-async function readShoppingList(filePath) {
-    try {
-        const fileData = await readFile(filePath, 'utf-8');
-        return JSON.parse(fileData);
-    } catch (error) {
-        // Si le fichier n'existe pas, retourne une liste de courses vide
-        return {};
+const personalShopper = (fileName, command, elem, quantity) => {
+    const filePath = `./${fileName}`;
+
+    // Create the file if it doesn't exist
+    if (!fs.existsSync(filePath) && command === 'create') {
+        fs.writeFileSync(filePath, '{}');
     }
-}
 
-// Fonction pour écrire la liste de courses dans le fichier JSON
-async function writeShoppingList(filePath, shoppingList) {
-    await writeFile(filePath, JSON.stringify(shoppingList, null, 2), 'utf-8');
-}
+    // Read the file contents
+    const fileContents = fs.readFileSync(filePath, 'utf8');
+    const shoppingList = JSON.parse(fileContents);
 
-// Fonction pour gérer les opérations d'ajout, de suppression et de listing
-async function manageShoppingList(filePath, command, elem, num = 1) {
-    let shoppingList = await readShoppingList(filePath);
-
+    // Execute the command
     switch (command) {
         case 'add':
             if (!elem) {
-                console.error('Erreur : No elem specified..');
+                console.error('No elem specified.');
                 return;
             }
-            shoppingList[elem] = (shoppingList[elem] || 0) + num;
-            break;
 
+            if (shoppingList[elem]) {
+                shoppingList[elem] += quantity || 1;
+            } else {
+                shoppingList[elem] = quantity || 1;
+            }
+
+            break;
         case 'rm':
             if (!elem) {
-                console.error('Erreur : No elem specified..');
+                console.error('No elem specified.');
                 return;
             }
-            if (isNaN(num)) {
-                console.error('Unexpected request: nothing has been removed');
-                return;
-            }
-            if (shoppingList[elem] !== undefined) {
-                shoppingList[elem] = Math.max(0, shoppingList[elem] - num);
-                if (shoppingList[elem] === 0) {
+
+            if (shoppingList[elem]) {
+                if (quantity) {
+                    shoppingList[elem] -= quantity;
+
+                    if (shoppingList[elem] <= 0) {
+                        delete shoppingList[elem];
+                    }
+                } else {
                     delete shoppingList[elem];
                 }
             }
+
             break;
-
-        case 'delete':
-            await unlink(filePath);
-            console.log(`Fichier ${filePath} supprimé.`);
-            return;
-
         case 'ls':
             if (Object.keys(shoppingList).length === 0) {
                 console.log('Empty list.');
                 return;
             }
-            Object.keys(shoppingList).forEach((item, index) => {
-                console.log(`- ${item} (${shoppingList[item]})`);
-            });
-            return;
 
+            for (const [key, value] of Object.entries(shoppingList)) {
+                console.log(`- ${key} (${value})`);
+            }
+
+            break;
         case 'help':
-            console.log('Commandes disponibles :');
-            console.log('- create <nom_fichier.json> : Crée un fichier JSON de liste de courses.');
-            console.log('- delete <nom_fichier.json> : Supprime le fichier JSON de liste de courses.');
-            console.log('- add <élément> [quantité] : Ajoute un élément à la liste de courses.');
-            console.log('- rm <élément> [quantité] : Supprime un élément de la liste de courses.');
-            console.log('- ls : Affiche la liste de courses.');
-            console.log('- help : Affiche cette aide.');
-            return;
+            console.log('Commands:');
+            console.log('  - create: takes a filename as argument and create it (should have `.json` extension specified)');
+            console.log('  - delete: takes a filename as argument and delete it');
+            console.log('  - add: takes an element name as argument and add it to the shopping list');
+            console.log('  - rm: takes an element name as argument and remove it from the shopping list');
+            console.log('  - ls: list the shopping list');
+            console.log('  - help: print this help message');
 
+            break;
         default:
-            console.error('Commande non reconnue. Utilisez "help" pour voir la liste des commandes disponibles.');
+            console.log('Invalid command.');
             return;
     }
 
-    await writeShoppingList(filePath, shoppingList);
-    console.log(`Liste de courses mise à jour :`);
-    Object.keys(shoppingList).forEach((item, index) => {
-        console.log(`- ${item} (${shoppingList[item]})`);
-    });
-}
+    // Write the file back to disk
+    fs.writeFileSync(filePath, JSON.stringify(shoppingList, null, 2));
+};
 
+const fileName = process.argv[2];
+const command = process.argv[3];
+const elem = process.argv[4];
+const quantity = Number(process.argv[5]);
 
-// Vérifier s'il y a suffisamment d'arguments
-if (process.argv.length < 3) {
-    console.error('Erreur :No elem specified.');
-    process.exit(1);
-}
-
-// Extraire la commande et les arguments
-const filePath = argv[2];
-const command = argv[3];
-const elem = argv[4];
-const num = parseInt(argv[5]);
-
-// Gérer la liste de courses en fonction de la commande
-manageShoppingList(filePath, command, elem, num);
+personalShopper(fileName, command, elem, quantity);
