caba0976e0ecffa0da8d961fef976d4ebf6d46d7
ahbarry <barryahmadou135@gmail.com>
2023-10-13 08:43:57 +0000

    ion-out.js

--- ion-out.js
+++ ion-out.js
@@ -3,6 +3,8 @@ const regexp = /\b\w+tion\b/g
 const rep = /ion/g
 
 const ionOut = (str) =>{
-    const match = str.match(regexp).map((value) => value.replace(rep, ''))
-    return match
+    const match = str.match(regexp)
+    return match === null ? [] : match.map((value) => value.replace(rep, ''))
 }
+
+console.log(ionOut('transfusion'))
