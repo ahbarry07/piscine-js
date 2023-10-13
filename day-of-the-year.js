e49145f9f5fb2c73f4049cb4badc6fe2c1825de3
ahbarry <barryahmadou135@gmail.com>
2023-10-13 09:09:04 +0000

    formattage

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,14 +1,14 @@
 
 
-function dayOfTheYear(date){
-    
+function dayOfTheYear(date) {
+
     let year = date.getFullYear()
-    if (date.getTime()=== (new Date(year, 0, 1)).getTime()){return 1}
+    if (date.getTime() === (new Date(year, 0, 1)).getTime()) { return 1 }
     let padStart = year.toString().padStart(4, '0')
-    let origineDate = (padStart+'-01-01')
+    let origineDate = (padStart + '-01-01')
     let origine = new Date(origineDate)
     const timeDifference = date - origine;
-    let numberOfday = Math.floor(timeDifference / (24*60*60*1000))
+    let numberOfday = Math.floor(timeDifference / (24 * 60 * 60 * 1000))
 
     return numberOfday + 1
 }
